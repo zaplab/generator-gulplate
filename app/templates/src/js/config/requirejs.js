eeacb3d4786cf5950fcb4d67aef659b12921d9cd
Sebastian Herber <sh@zaplab.at>
2015-12-09 00:58:56 +0100

    [CLEANUP] Removes requirejs config template file

--- app/templates/src/js/config/requirejs.js
+++
@@ -1,9 +0,0 @@
-
-if (typeof require !== 'undefined' && require.config) {
-    require.config({
-        paths: {
-            'main': 'src/js/main',
-            'module-a': 'src/js/module-a'
-        }
-    });
-}
